0e4af98b5efd053d4857e0eddf5251c9f6c4f6a0
a21sahfa <a21sahfa@student.his.se>
2023-05-03 11:43:12 +0200

    In MainActivity: Read data from Shared Preferences Create a new screen called SecondActivity that can be opened from MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +15,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+
+        SharedPreferences myPreferenceRef = getSharedPreferences("MyPreferencesName", MODE_PRIVATE);
+        SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+
+        TextView snygg;
+
+
+            super.onCreate(savedInstanceState);
+            setContentView((R.layout.secondactivity));
+            snygg = findViewById(R.id.sahib);
+
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                String name = extras.getString("name");
+                int number = extras.getInt("number");
+                // Do something with the name and number
+
+                snygg.setText(name + number);
+
+
+
+        }
+        }
 
 }
