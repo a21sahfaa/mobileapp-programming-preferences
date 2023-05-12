d65fa8990703c83cdc7cc7604a8ed8933b490502
a21sahfa <a21sahfa@student.his.se>
2023-05-12 16:39:20 +0200

    skapade ny activity och la till shared preferences, textview och button

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,45 +1,42 @@
 package com.example.project;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences myPreferenceRef;
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onresumemain1", "onCreate()");
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        SharedPreferences myPreferenceRef = getSharedPreferences("MyPreferencesName", MODE_PRIVATE);
-        SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
-        myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
-
-
-        TextView snygg;
-
-
-            super.onCreate(savedInstanceState);
-            setContentView((R.layout.secondactivity));
-            snygg = findViewById(R.id.sahib);
-
-            Bundle extras = getIntent().getExtras();
-            if (extras != null) {
-                String name = extras.getString("name");
-                int number = extras.getInt("number");
-                // Do something with the name and number
-
-                snygg.setText(name + number);
-
-
-
-        }
-        }
-
+        setContentView(R.layout.activity_main2);
+        button = findViewById(R.id.prefButton);
+        textView = findViewById(R.id.prefText);
+        myPreferenceRef = getSharedPreferences("ref", MODE_PRIVATE);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        Log.d("onresumemain2", "onResume()");
+        super.onResume();
+        String savedText = myPreferenceRef.getString("savepref", "not found");
+        textView.setText(savedText);
+    }
 }
+
+
